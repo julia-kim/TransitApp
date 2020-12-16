d7213aa99830be51a0ba2ffdb62939f4e2d5cdda
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2020-12-16 15:29:14 -0500

    Customize map marker with bus pin

--- src/main/resources/static/bus-map.js
+++ src/main/resources/static/bus-map.js
@@ -1,16 +1,18 @@
-var map;
+var map
 function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: { lat: parseFloat(busLocations[0].LATITUDE), lng: parseFloat(busLocations[0].LONGITUDE) },
-		zoom: 15,
-		scrollwheel: false
-	});
-	
-	for (i=0; i<busLocations.length; i++){
-		var marker = new google.maps.Marker({
-		    position: { lat: parseFloat(busLocations[i].LATITUDE), lng: parseFloat(busLocations[i].LONGITUDE) },
-		    map: map,
-		});
-	}
-}
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: parseFloat(busLocations[0].LATITUDE), lng: parseFloat(busLocations[0].LONGITUDE) },
+    zoom: 15,
+    scrollwheel: false,
+  })
 
+  var image = { url: "/bus_marker.png", scaledSize: new google.maps.Size(50, 50) }
+
+  for (i = 0; i < busLocations.length; i++) {
+    var marker = new google.maps.Marker({
+      position: { lat: parseFloat(busLocations[i].LATITUDE), lng: parseFloat(busLocations[i].LONGITUDE) },
+      map: map,
+      icon: image,
+    })
+  }
+}
