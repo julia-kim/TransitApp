189b366780679ad2a8ac5bd561c3d5000d6d44fb
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2020-12-17 15:24:50 -0500

    Open info window with bus number when user clicks on bus marker

--- src/main/resources/static/bus-map.js
+++ src/main/resources/static/bus-map.js
@@ -6,13 +6,30 @@ function initMap() {
     scrollwheel: false,
   })
 
-  var image = { url: "/bus_marker.png", scaledSize: new google.maps.Size(50, 50) }
+  var bus = { url: "/bus-marker.png", scaledSize: new google.maps.Size(50, 50) }
+  var person = { url: "/person-marker.png", scaledSize: new google.maps.Size(50, 50) }
 
   for (i = 0; i < busLocations.length; i++) {
-    var marker = new google.maps.Marker({
+    let marker = new google.maps.Marker({
       position: { lat: parseFloat(busLocations[i].LATITUDE), lng: parseFloat(busLocations[i].LONGITUDE) },
       map: map,
-      icon: image,
+      icon: bus,
+    })
+
+    let busNumberContentString = "<h2>Bus No. " + busLocations[i].VEHICLE + "</h2>"
+
+    let infowindow = new google.maps.InfoWindow({
+      content: busNumberContentString,
+    })
+
+    google.maps.event.addListener(marker, "click", function () {
+      infowindow.open(map, marker)
     })
   }
+  /*	let personMarker = new google.maps.Marker({
+		position: centerCoords,
+		map: map,
+		icon: person,
+		animation: google.maps.Animation.BOUNCE
+		});*/
 }
